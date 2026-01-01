6e385dbeb7f953f2e9363c26e82c5a8ccedfd370
Tajudeen Fawas <frankokonkwo24@gmail.com>
2026-01-01 22:30:23 +0100

    working on the login form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,82 +1,15 @@
-"use client";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
-
-
-// Schema
+"use client"
+import {z} from "zod"
+//Schema -> 
 const loginSchema = z.object({
-  email: z.email("Please enter a valid email address!"),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
-});
+  email: z.email("please enter a valid email address!"),
+  password: z.string().min(6, "Password must be at least 6 character long")
+})
 
-type LoginFormValues = z.infer<typeof loginSchema>;
+type LoginForm = z.infer<typeof loginSchema>
 
 function LoginForm() {
-  const [isLoading, setIsLoading] = useState(false);
-
-  const form = useForm<LoginFormValues>({
-      resolver: zodResolver(loginSchema),
-      defaultValues: {
-          email: "",
-          password: ""
-      }
-  });
-    
-    const onLoginSubmit = async (values: LoginFormValues) => {
-        setIsLoading(true)
-        try {
-            console.log(values);
-            
-        } catch (error) {
-            
-        }
-    }
-
-    return (
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onLoginSubmit)} className="space-y-4">
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter your email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input
-                    type="password"
-                    placeholder="Enter your Password"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <Button type="submit" className="w-full" disabled={isLoading}>
-            {isLoading ? "Signing in ... " : "Sign In"}
-          </Button>
-        </form>
-      </Form>
-    );
+  return <div></div>
 }
 
-export default LoginForm;
+export default LoginForm
