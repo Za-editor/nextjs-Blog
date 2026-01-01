edbdde4e8f59fd725e49115974393f71746ad9dc
Tajudeen Fawas <frankokonkwo24@gmail.com>
2026-01-01 22:41:58 +0100

    worked on the login input fields

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,15 +1,64 @@
-"use client"
-import {z} from "zod"
-//Schema -> 
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { Input } from "../ui/input";
+//Schema ->
 const loginSchema = z.object({
   email: z.email("please enter a valid email address!"),
-  password: z.string().min(6, "Password must be at least 6 character long")
-})
+  password: z.string().min(6, "Password must be at least 6 character long"),
+});
 
-type LoginForm = z.infer<typeof loginSchema>
+type LoginFormvalues = z.infer<typeof loginSchema>;
 
 function LoginForm() {
-  return <div></div>
+  const { isLoading, setIsLoading } = useState(false);
+
+  //initialize form
+  const form = useForm<LoginFormvalues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Enter your email" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        ></FormField>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Enter your Password"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        ></FormField>
+      </form>
+    </Form>
+  );
 }
 
-export default LoginForm
+export default LoginForm;
